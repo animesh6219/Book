aa73e842015156cf36acf2d4b8cc700c06f95ef3
animesh6219 <62498877+animesh6219@users.noreply.github.com>
2025-07-22 19:30:15 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('hindi-story-cache').then(cache => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './manifest.json',
+        './icon-192.png',
+        './icon-512.png'
+      ]);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response =>
+      response || fetch(event.request)
+    )
+  );
+});
